007946f537fea5b33b25c43ef735113af6fe2dbd
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-11-08 11:32:07 +0700

    adjust request's json limit

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -19,8 +19,8 @@ app.set('trust proxy', CONFIG.TRUST_PROXY);
 if (CONFIG.CSP.ENABLED) app.use(baseCSP);
 app.use(cors());
 app.use('/resources', express.static(path.join(__dirname, '/public')));
-app.use(express.json({ limit: '32mb' }));
-app.use(express.urlencoded({ extended: true, limit: '32mb' }))
+app.use(express.json({ limit: '500kb' }));
+app.use(express.urlencoded({ extended: true, limit: '500kb' }))
 
 
 // Custom Middlewares
