f07f4c8e8baf9eeab90f37a64d0ce6d0ee964972
NGHIA.NV190060@sis.hust.edu.vn <NGHIA.NV190060@sis.hust.edu.vn>
2022-11-13 12:26:41 +0700

    upd: bad request error

--- back-end/src/errors/http-errors/bad-request.ts
+++ back-end/src/errors/http-errors/bad-request.ts
@@ -1,20 +1,28 @@
-import { RESULT, ERROR_VI } from "../../types/enum";
+import { ValidationError } from 'express-validator';
+
 import { BaseHttpError } from "./base-http-error";
+import { RESULT, ERROR_VI } from "../../types/enum";
 
 
 export class BadRequestError extends BaseHttpError {
     statusCode = 400;
   
-    constructor(message?: string) {
-        super(message || ERROR_VI.MISSING_PARAMETERS);
+    constructor(public errors: ValidationError[]) {
+        super(ERROR_VI.MISSING_PARAMETERS);
 
         Object.setPrototypeOf(this, BadRequestError.prototype);
     }
   
     respond() {
+        let message = ''
+
+        for (const e of this.errors) {
+            message += `; ${e.msg}`
+        }
+
         return {
             result: RESULT.fail,
-            message: this.message,
+            message: message,
         };
     }
 }
